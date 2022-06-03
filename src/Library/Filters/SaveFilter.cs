4151953c251d55fa55c9723a07929b1a7143a7c7
Leandro Alfonso <leamanya24.6@gmail.com>
2022-06-02 21:05:35 -0300

    Ejercicios 1 y 2 finalizados

---
+++ src/Library/Filters/SaveFilter.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace CompAndDel.Filters
+{
+    public class SaveFilter : IFilter
+    {
+        private string id;
+        
+        public SaveFilter(string id)
+        {
+            this.id = id;
+        }
+        
+        /// Un filtro que retorna el negativo de la imagen recibida.
+        /// </summary>
+        /// <param name="image">La imagen a la cual se le va a aplicar el filtro.</param>
+        /// <returns>La imagen recibida pero en negativo.</returns>
+        public IPicture Filter(IPicture image)
+        {
+            var pProvider = new PictureProvider();
+            pProvider.SavePicture(image, Path.Combine("..", "..", "..", "..", "..","resultados", "result_"+id+".jpg"));
+            return image;
+        }
+    }
+}
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using CompAndDel.Filters;
 using CompAndDel.Pipes;
-using CompAndDel.Filters;
+using Path = System.IO.Path;
 
 namespace CompAndDel
 {
@@ -8,6 +8,18 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            string prePath = Path.Combine("..", "..", "..");
+            var pProvider = new PictureProvider();
+            IPicture beer = pProvider.GetPicture(Path.Combine(prePath, "beer.jpg"));
+            var pipeSerial = new PipeSerial(
+                new FilterGreyscale(), new PipeSerial(
+                    new SaveFilter("mitad"), new PipeSerial(
+                        new FilterNegative(), new PipeNull()
+                        )
+                    )
+                );
+            var resultadoBeer = pipeSerial.Send(beer);
+            pProvider.SavePicture(resultadoBeer, Path.Combine(prePath, "..", "..", "resultados", "resultado_beer.jpg"));
         }
     }
 }
